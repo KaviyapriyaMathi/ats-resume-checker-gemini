2251980b1c978b3375974e1ff95180381f8304f0
Kaviya Priya M <kaviyapriyamathiyazhagan@gmail.com>
2025-10-08 09:34:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 # ----------------------------
 load_dotenv(".env")
 genai.configure(api_key=os.getenv("API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-1.5-flash-latest")
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
